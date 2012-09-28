85ffc12f3ac7b5964f82c49b0fe91c9fb6ae3441
Erik Faye-Lund <kusmabite@gmail.com>
2012-09-28 10:20:15 +0200

    rewrite tgrdma logic while fixing mask and other instructions

--- rnn/tgrdma.c
+++ rnn/tgrdma.c
@@ -14,6 +14,7 @@ static void usage()
 FILE *fp;
 static char *line;
 static size_t line_len;
+static int echo_comments = 1, echo_commands = 0, echo_data = 0;
 
 static int get_next_word(unsigned long *dst)
 {
@@ -24,7 +25,8 @@ restart:
 		return ret;
 
 	if (line_len > 1 && *line == '#') {
-		fputs(line, stdout);
+		if (echo_comments)
+			fputs(line, stdout);
 		goto restart;
 	}
 
@@ -37,18 +39,58 @@ restart:
 	return 0;
 }
 
+static unsigned long get_next_value(void)
+{
+	unsigned long value;
+
+	if (get_next_word(&value)) {
+		fprintf(stderr, "unexpected end of file!\n");
+		exit(1);
+	}
+
+	if (echo_data)
+		printf("# 0x%08lx\n", value);
+
+	return value;
+}
+
+static struct rnndeccontext *vc;
+static struct rnndb *db;
+struct rnndomain *dom;
+
+static void handle_write(int reg, unsigned long val)
+{
+	struct rnndecaddrinfo *info = rnndec_decodeaddr(vc, dom, reg, 0);
+
+	if (info && info->typeinfo)
+		printf("\t%s => %s\n", info->name, rnndec_decodeval(vc, info->typeinfo, val, info->width));
+	else if (info)
+		printf("\t%s => 0x%lx\n", info->name, val);
+	else
+		printf("\t0x%x => 0x%lx\n", reg, val);
+}
+
 int main(int argc, char *argv[])
 {
 	int c;
 	char *file = NULL;
 	unsigned long x;
-	struct rnndeccontext *vc;
-	struct rnndb *db;
 
-	while ((c = getopt(argc, argv, "f:")) != -1) {
+	while ((c = getopt(argc, argv, "f:en")) != -1) {
 		switch (c) {
 		case 'f':
 			file = strdup(optarg);
+			break;
+
+		case 'e':
+			echo_commands = 1;
+			echo_data = 1;
+			break;
+
+		case 'n':
+			echo_comments = 0;
+			echo_commands = 0;
+			echo_data = 0;
 			break;
 
 		default:
@@ -66,7 +108,7 @@ int main(int argc, char *argv[])
 	rnn_prepdb(db);
 	vc = rnndec_newcontext(db);
 	vc->colors = &envy_def_colors;
-	struct rnndomain *dom = rnn_finddomain(db, "TGR3D");
+	dom = rnn_finddomain(db, "TGR3D");
 	if (!dom) {
 		fprintf(stderr, "Could not find domain TGR3D\n");
 		exit(1);
@@ -84,59 +126,73 @@ int main(int argc, char *argv[])
 
 	while (get_next_word(&x) >= 0) {
 		unsigned long value;
-		int op, offset, address, class, mask, size, i;
+		int op, offset, class_id, mask, count, i;
 
-		/* decode instruction */
+		if (echo_commands)
+			printf("# 0x%08lx\n", x);
+
                 op = (x & 0xf0000000) >> 28;
-		offset = op != 5 ? (x & 0x0fff0000) >> 16 : 0;
-		address = op == 3 ? (x & 0x0fffffff) >> 4 : 0; 
-		class = op == 0 ? (x & 0xffc0) >> 6 : 0;
-		mask = op == 0 ? (x & 0x3f) : 0;
-		size = (op == 1 || op == 2 || op == 6) ? x & 0xffff : 0;
-		value = op == 4 ? x & 0xffff : 0;
 
+		/* decode instructions */
 		switch (op) {
 		case 0:
-			printf("setclass 0x%X 0x%X 0x%X\n", class, offset, mask);
-			continue;
+			offset = (x & 0x0fff0000) >> 16;
+			class_id = (x & 0xffc0) >> 6;
+			mask = x & 0x3f;
+			printf("setclass 0x%x 0x%x 0x%x\n", offset, class_id, mask);
+			break;
 
 		case 1:
-			printf("incr 0x%X 0x%X\n", offset, size);
+			offset = (x & 0x0fff0000) >> 16;
+			count = x & 0xffff;
+			printf("incr 0x%x 0x%x\n", offset, count);
+			for (i = 0; i < count; ++i)
+				handle_write(offset + i, get_next_value());
 			break;
 
 		case 2:
-			printf("nonincr 0x%X 0x%X\n", offset, size);
+			offset = (x & 0x0fff0000) >> 16;
+			count = x & 0xffff;
+			printf("nonincr 0x%x 0x%x\n", offset, count);
+			for (i = 0; i < count; ++i)
+				handle_write(offset, get_next_value());
 			break;
 
 		case 3:
-			printf("restart 0x%X\n", address);
+			offset = (x & 0x0fff0000) >> 16;
+			mask = x & 0xffff;
+			printf("mask 0x%x 0x%x\n", offset, mask);
+
+			for (i = 0; i < 32; ++i) {
+				if (!(mask & (1 << i)))
+					continue;
+				handle_write(offset + i, get_next_value());
+			}
 			break;
 
 		case 4:
-			printf("imm 0x%X 0x%lX\n", offset, value);
-			size = 1; /* hack: make sure we enter the loop */
+			offset = (x & 0x0fff0000) >> 16;
+			value = x & 0xffff;
+			printf("imm 0x%x 0x%lx\n", offset, value);
+			handle_write(offset, value);
 			break;
+
+#if 0
+		case 5:
+			address = op == 5 ? (x & 0x0fffffff) >> 4 : 0; 
+			printf("restart 0x%x\n", address);
+			break;
+
+		case 6:
+			offset = (x & 0x0fff0000) >> 16;
+			count = x & 0xffff;
+			printf("gather (unfinished) 0x%x 0x%x\n", offset, count);
+			break;
+#endif
 
 		default:
 			fprintf(stderr, "unknown opcode %d (%08lx)\n", op, x);
 			exit(1);
-		}
-
-		for (i = 0; i < size; ) {
-			struct rnndecaddrinfo *info = rnndec_decodeaddr(vc, dom, op != 2 ? offset + i : offset, 0);
-			if (op != 4 && get_next_word(&value)) {
-				fprintf(stderr, "unexpected end of file!\n");
-				exit(1);
-			}
-
-			if (info && info->typeinfo)
-				printf("\t%s => %s\n", info->name, rnndec_decodeval(vc, info->typeinfo, value, info->width));
-			else if (info)
-				printf("\t%s => 0x%lX\n", info->name, value);
-			else
-				printf("\t0x%X => 0x%lX\n", offset, value);
-
-			++i;
 		}
 	}
 
